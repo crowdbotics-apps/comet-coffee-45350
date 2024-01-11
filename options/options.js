34986971a8787d0155d9ee5017c4d14bdfa9b2e5
Crowdbotics <team@crowdbotics.com>
2024-01-11 02:22:45 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "comic-coffee-45350",
+  url: "https://comic-coffee-45350.botics.co",
+  api: "https://comic-coffee-45350.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
